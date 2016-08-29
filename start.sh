364bd553cf3055d8f7738fb766d38d0700f9260b
Andrew Pearson <andrew@andrewpe.com>
2016-08-29 16:57:19 -0500

    Changes paths on start script

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 #Starting Main JS
-pm2 start /srv/test/ACM-Payment/node/main.js --name "ACM-Payment-Server" --log-date-format "MM-DD-YYYY HH:mm:ss" --watch --ignore-watch logs --output "/srv/test/ACM-Payment/node/logs/server.log" --error "/srv/test/ACM-Payment/node/logs/error.log"
+pm2 start ./main.js --name "ACM-Payment-Server" --log-date-format "MM-DD-YYYY HH:mm:ss" --watch --ignore-watch logs --output "./logs/server.log" --error "./logs/error.log"
