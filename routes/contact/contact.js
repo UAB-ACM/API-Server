f6518a7d7bcba6766502bbce499bcbd322a3e0e4
Andrew Pearson <andrew@andrewpe.com>
2016-09-27 14:13:02 -0500

    Adding email to contact form

--- routes/contact/contact.js
+++ routes/contact/contact.js
@@ -1,12 +1,29 @@
 //Switching to Strict JS
 'use strict';                                                                   //Converts mistakes to errors
+var config = require('../../config.json');                                      //Config
+
+
+var nodemailer = require('nodemailer');                                         //Mailer Library
+var transportString = `smtps://${config.email.username}%40${config.email.domain}:${config.email.password}@${config.email.smtpServer}`
+var transporter = nodemailer.createTransport(transportString);                  //Open SMTP Connection
+
 
 //Exporting route
 module.exports = function (req, res) {                                          //Export the function that is used to handle the web request
-  console.log(req.body.name);
-  console.log(req.body.email);
-  console.log(req.body.message);
-  console.log();
+  console.log('New Contact Request');
+  var mailOptions = {                                                         //Mail Config
+      from: '"ACM Server" <uabacm@gmail.com>',                                  // sender address
+      to: 'acm@cis.uab.edu',                                                    // list of receivers
+      subject: 'New Contact Form!',                                               // Subject line
+      text: `Name: ${req.body.name} \n Email: ${req.body.email} \n Message: ${req.body.message}`,
+  };
 
-  res.send('OK');                                                               //Send data object back to client with code (200)
+  transporter.sendMail(mailOptions, function(error, info){                    //Send the mail
+      if(error){
+          res.status(500).send('Email Not Sent');
+      }
+      console.log('Message sent: ' + info.response);
+      res.send('Message Sent');                                                               //Send data object back to client with code (200)
+
+  });
 };
